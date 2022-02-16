be6c460c688bf4b6c167f7b9fb64b32a9f609db7
Yura Lysyshak <yuralysyshak@gmail.com>
2022-02-16 23:04:47 +0200

    Tip

---
+++ app/src/main/java/com/bawp/jettip_test/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.bawp.jettip_test.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB0080)
+val Purple500 = Color(0xFFF0F30B3)
+val Purple700 = Color(0xFFF0F30B3)
+val Teal200 = Color(0xFFF0EEF3)
+
+val backgroundColor = Color(0xFFF0EEF3)
+val darkTextColor = Color(0xFFF0EEF3)
+val lightTextColor = Color.Gray
+val foregroundColor = Color(0xFFF0EEF3)
+val accentColor = Color(0xFFF0EEF3)
